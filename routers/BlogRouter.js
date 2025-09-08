e348dedd5c9518b0637a158cfde378997be78ad9
MARIENKOW <den.marienkow@gmail.com>
2025-09-08 05:03:57 +0300

    fix

--- routers/BlogRouter.js
+++ routers/BlogRouter.js
@@ -5,13 +5,13 @@ import { v4 } from "uuid";
 import multer from "multer";
 
 const storage = multer.diskStorage({
-   destination: function (req, file, callback) {
-      callback(null, "./uploads/nft/");
-   },
-   filename: function (req, file, callback) {
-      req.fileName = v4() + file.originalname;
-      callback(null, req.fileName);
-   },
+    destination: function (req, file, callback) {
+        callback(null, "./uploads/nft/");
+    },
+    filename: function (req, file, callback) {
+        req.fileName = v4() + file.originalname;
+        callback(null, req.fileName);
+    },
 });
 
 const upload = multer({ storage }).single("img");
@@ -20,6 +20,14 @@ const BlogRouter = new Router();
 
 BlogRouter.post("/", autAdminMiddelware, BlogController.create);
 BlogRouter.get("/", BlogController.getAll);
+BlogRouter.get("/main", BlogController.getMain);
+BlogRouter.get("/short", BlogController.getShort);
+BlogRouter.get("/important", BlogController.getImportant);
+BlogRouter.put(
+    "/important/:id",
+    autAdminMiddelware,
+    BlogController.setImportant
+);
 BlogRouter.get("/:id", BlogController.getById);
 BlogRouter.delete("/:id", autAdminMiddelware, BlogController.delete);
 BlogRouter.put("/:id", autAdminMiddelware, BlogController.update);
